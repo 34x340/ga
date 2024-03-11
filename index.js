e3243c611ff91f68200ba4640f28e0ed728c32c5
34x340 <114092241+34x340@users.noreply.github.com>
2024-03-11 19:15:31 +0200

    sp

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const numbers = Array.from(document.querySelectorAll('.input-num__list')).map(el => parseInt(el.textContent));
-const highestNumber = Math.max(...numbers);
+// const numbers = Array.from(document.querySelectorAll('.input-num__list')).map(el => parseInt(el.textContent));
+// const highestNumber = Math.max(...numbers);
 
-console.log(highestNumber, '.highest-number__text');
+// console.log(highestNumber, '.highest-number__text');
+
+function calculateTime() {
+    const hours = parseInt(document.getElementById('hours').value) || 0;
+    const minutes = parseInt(document.getElementById('minutes').value) || 0;
+    const seconds = parseInt(document.getElementById('seconds').value) || 0;
+  
+    const totalSeconds = hours * 3600 + minutes * 60 + seconds;
+  
+    const hoursResult = Math.floor(totalSeconds / 3600);
+    const minutesResult = Math.floor((totalSeconds % 3600) / 60);
+    const secondsResult = totalSeconds % 60;
+  
+    document.getElementById('start-timer').innerText = `Time: ${hoursResult}h ${minutesResult}m ${secondsResult}s`;
+  }
