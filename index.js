236e59e0c38fc0109e92557fc81dc29f14a3c786
34x340 <114092241+34x340@users.noreply.github.com>
2024-03-13 19:14:09 +0200

    result

--- index.js
+++ index.js
@@ -3,16 +3,42 @@
 
 // console.log(highestNumber, '.highest-number__text');
 
-function calculateTime() {
-    const hours = parseInt(document.getElementById('hours').value) || 0;
-    const minutes = parseInt(document.getElementById('minutes').value) || 0;
-    const seconds = parseInt(document.getElementById('seconds').value) || 0;
+// function calculateTime() {
+//     const hours = parseInt(document.getElementById('hours').value) || 0;
+//     const minutes = parseInt(document.getElementById('minutes').value) || 0;
+//     const seconds = parseInt(document.getElementById('seconds').value) || 0;
   
-    const totalSeconds = hours * 3600 + minutes * 60 + seconds;
+//     const totalSeconds = hours * 3600 + minutes * 60 + seconds;
   
-    const hoursResult = Math.floor(totalSeconds / 3600);
-    const minutesResult = Math.floor((totalSeconds % 3600) / 60);
-    const secondsResult = totalSeconds % 60;
+//     const hoursResult = Math.floor(totalSeconds / 3600);
+//     const minutesResult = Math.floor((totalSeconds % 3600) / 60);
+//     const secondsResult = totalSeconds % 60;
   
-    document.getElementById('start-timer').innerText = `Time: ${hoursResult}h ${minutesResult}m ${secondsResult}s`;
-  }
+//     document.getElementById('start-timer').innerText = `Time: ${hoursResult}h ${minutesResult}m ${secondsResult}s`;
+//   }
+
+// КАЛЬКУЛЯТОР !!!!!!!!!!!!!!
+
+// function calculate() {
+//   let result = document.getElementById('result-input');
+//   let firstNum = document.getElementById('first-input').value;
+//   let calculate = document.getElementById('op').value;
+//   let secondNum = document.getElementById('second-input').value;
+//   if(calculate === '+')
+//   {
+//     result = firstNum + secondNum;
+//   }
+//   elseif(calculate === '-')
+//   {
+//     result = firstNum - secondNum;
+//   }
+//   elseif(calculate === '*')
+//   {
+//     result = firstNum * secondNum;
+//   }
+//   elseif(calculate === '/')
+//   {
+//     result = firstNum / secondNum;
+//   }
+//   document.getElementById('result').innerHTML = D;
+// }
