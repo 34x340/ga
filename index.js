65bc9d5c44208ccc8d9dda8584e89001414204e9
34x340 <114092241+34x340@users.noreply.github.com>
2024-03-20 18:15:24 +0200

    sections

--- index.js
+++ index.js
@@ -123,90 +123,172 @@ const choices = ["rock", "paper", "scissors"];
     // ВЧЕНІ ВЧЕНІ ВЧЕНІ ВЧЕНІ ВЧЕНІ ВЧЕНІ ВЧЕНІ ВЧЕНІ ВЧЕНІ ВЧЕНІ ВЧЕНІ ВЧЕНІ ВЧЕНІ ВЧЕНІ ВЧЕНІ 
     
     const scientists = [ 
-        {
-            name: "Marie",
-            surname: "Curie",
-            born: 1867,
-            dead: 1934
-        },
-        {
-            name: "Charles",
-            surname: "Darwin",
-            born: 1809,
-            dead: 1882
-        },
-        {
-            name: "Alexander",
-            surname: "Fleming",
-            born: 1881,
-            dead: 1955
-        },
-        {
-            name: "Gregor",
-            surname: "Mendel",
-            born: 1822,
-            dead: 1884
-        },
-        {
-            name: "Thomas",
-            surname: "Edison",
-            born: 1847,
-            dead: 1931
-        },
-        {
-            name: "Albert",
-            surname: "Einstein",
-            born: 1879,
-            dead: 1955
-        },
-        {
-            name: "Nikola",
-            surname: "Tesla",
-            born: 1856,
-            dead: 1943
-        },
-        {
-            name: "Louis",
-            surname: "Pasteur",
-            born: 1822,
-            dead: 1895
-        },
-        {
-            name: "Michael",
-            surname: "Faraday",
-            born: 1791,
-            dead: 1867
-        },
-        {
-            name: "William",
-            surname: "Herschel",
-            born: 1738,
-            dead: 1822
-        },
-        {
-            name: "Galileo",
-            surname: "Galilei",
-            born: 1564,
-            dead: 1642
-        },
-        {
-            name: "Jacques",
-            surname: "Curie",
-            born: 1859,
-            dead: 1906
-        }
+        { 
+            name: "Albert", 
+            surname: "Einstein", 
+            born: 1879, 
+            dead: 1955, 
+            id: 1 
+        }, 
+        { 
+            name: "Isaac", 
+            surname: "Newton", 
+            born: 1643, 
+            dead: 1727, 
+            id: 2 
+        }, 
+        { 
+            name: "Galileo", 
+            surname: "Galilei", 
+            born: 1564, 
+            dead: 1642, 
+            id: 3 
+        }, 
+        { 
+            name: "Marie", 
+            surname: "Curie", 
+            born: 1867, 
+            dead: 1934, 
+            id: 4 
+        }, 
+        { 
+            name: "Johannes", 
+            surname: "Kepler", 
+            born: 1571, 
+            dead: 1630, 
+            id: 5 
+        }, 
+        { 
+            name: "Nicolaus", 
+            surname: "Copernicus", 
+            born: 1473, 
+            dead: 1543, 
+            id: 6 
+        }, 
+        { 
+            name: "Max", 
+            surname: "Planck", 
+            born: 1858, 
+            dead: 1947, 
+            id: 7 
+        }, 
+        { 
+            name: "Katherine", 
+            surname: "Blodgett", 
+            born: 1898, 
+            dead: 1979, 
+            id: 8 
+        }, 
+        { 
+            name: "Ada", 
+            surname: "Lovelace", 
+            born: 1815, 
+            dead: 1852, 
+            id: 9 
+        }, 
+        { 
+            name: "Sarah E.", 
+            surname: "Goode", 
+            born: 1855, 
+            dead: 1905, 
+            id: 10 
+        }, 
+        { 
+            name: "Lise", 
+            surname: "Meitner", 
+            born: 1878, 
+            dead: 1968, 
+            id: 11 
+        }, 
+        { 
+            name: "Hanna", 
+            surname: "Hammarström", 
+            born: 1829, 
+            dead: 1909, 
+            id: 12 
+        } 
     ];
-
+    
     const prompts = [
-        'Які вчені народилися в 19 ст.',
-        'Відсортувати вчених за алфавітом',
-        'Відсортувати вчених за кількістю прожитих років',
-        'Знайти вченого, який народився найпізніше',
+        'отримати масив вчених що народилися в 19 ст',
+        // 'знайти суму років скільки прожили всі вченнів',
+        'Відсортувати вчених по алфавіту',
+        'Відсортувати вчених по кількості прожитих років',
+        // 'Видалити з масива вчених що народилися в 15 або 16 або 17 столітті',
+        'Знайти вченого який народився найпізніше.',
         'Знайти рік народження Albert Einstein',
         'знайти вчених прізвище яких починається на літеру С',
         'Видалити з масива всіх вчених імя яких починається на A',
         'Знайти вченого який прожив найбільше і вченого який прожив найменьше',
         'Знайти вчених в яких співпадають перші літери імені і прізвища',
+        // 'Дізнатися чи всі вченні працювали в 19 столітті',
     ];
-
     
+    const scientistsEl = document.querySelector('.info__scientists');
+    
+    scientists.forEach((scientist) => scientistsEl.innerHTML += `
+        <li class="info__scientist" data-id="${scientist.id}">
+            <p class="info__name">${scientist.name} ${scientist.surname}</p>
+            <p class="info__lifeYears">${scientist.born}-${scientist.dead} years</p>
+        </li>
+    `);
+    
+    const promptsEl = document.querySelector('.info__prompts');
+    
+    prompts.forEach((scientist, idx) => promptsEl.innerHTML += `
+        <li class="info__prompt">
+            <button class="info__but" data-promptNum="${idx}">${scientist}</button>
+        </li>
+    `);
+    
+    const showScientists = (scientistsToShow) => {
+        scientistsEl.innerHTML = '';
+        if (Array.isArray(scientistsToShow)) {
+            scientistsToShow.forEach((scientistId) => scientistsEl.innerHTML += `
+                <li class="info__scientist" data-id="${scientistId}">
+                    <p class="info__name">${scientists.find(scientist => scientist.id === scientistId).name} ${scientists.find(scientist => scientist.id === scientistId).surname}</p>
+                    <p class="info__lifeYears">${scientists.find(scientist => scientist.id === scientistId).born}-${scientists.find(scientist => scientist.id === scientistId).dead} years</p>
+                </li>
+            `);
+        }
+        else {
+            scientistsEl.innerHTML += `
+                <li class="info__scientist" data-id="${scientistsToShow}">
+                    <p class="info__name">${scientistsToShow.name} ${scientistsToShow.surname}</p>
+                    <p class="info__lifeYears">${scientistsToShow.born}-${scientistsToShow.dead} years</p>
+                </li>
+            `;
+        }
+    }
+    
+    promptsEl.addEventListener('click', (e) => {
+        switch (e.target.getAttribute('data-promptNum')) {
+            case '0':
+                showScientists(scientists.filter(scientist => scientist.born > 1800 && scientist.born <= 1900).map(scientist => scientist.id));
+                break;
+            case '1':
+                showScientists(scientists.sort((a, b) => a.name.localeCompare(b.name)).map(scientist => scientist.id));
+                break;
+            case '2':
+                showScientists(scientists.sort((a, b) => (b.dead-b.born) - (a.dead-a.born)).map(scientist => scientist.id));
+                break;
+            case '3':
+                showScientists(scientists.sort((a, b) => b.born - a.born)[0]);
+                break;
+            case '4':
+                showScientists(scientists.find((scientist) => scientist.name === 'Albert' && scientist.surname === 'Einstein'));
+                break;
+            case '5':
+                showScientists(scientists.filter(scientist => scientist.surname.startsWith('C')).map(scientist => scientist.id));
+                break;
+            case '6':
+                showScientists(scientists.filter(scientist => !(scientist.name.startsWith('A'))).map(scientist => scientist.id));
+                break;
+            case '7':
+                showScientists([Math.max(...scientists.map(scientist => scientist.dead - scientist.born)), Math.min(...scientists.map(scientist => scientist.dead - scientist.born))].map(lifeYears => scientists.find(scientist => scientist.dead - scientist.born === lifeYears).id));
+                break;
+            case '8':
+                showScientists(scientists.filter(scientist => scientist.name[0] === scientist.surname[0]).map(scientist => scientist.id));
+                break;
+        }
+    });
